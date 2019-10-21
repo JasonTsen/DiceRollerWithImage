0e74311e77e224b931b4080ab162b1f7f4732450
Jason Tsen <sjtsen98@gmail.com>
2019-10-21 23:25:16 +0800

    dice roller new

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,13 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.Id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt( bound: 6) + 1
+        val randomInt = Random().nextInt(6) + 1
 
+
+
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
     }
