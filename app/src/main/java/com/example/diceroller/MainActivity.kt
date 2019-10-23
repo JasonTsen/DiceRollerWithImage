4534ace81b7d6a2ccd6da29b407bf9551ed3c392
Jason Tsen <sjtsen98@gmail.com>
2019-10-23 23:22:41 +0800

    DiceRollerImage

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,23 +10,26 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-
+        val randomInt2 = Random().nextInt( 6 ) + 1
 /*
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()*/
         val diceImage: ImageView = findViewById(R.id.dice_image)
+        val diceImage2: ImageView = findViewById(R.id.dice_image2)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -36,6 +39,17 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
 
         }
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
     }
+
 }
